c2990614f418fae944dc38e7d81b53d7fceba095
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-01-29 10:00:33 -0500

    Add SessionId() to Session interface.

--- session/session.go
+++ session/session.go
@@ -1,32 +1,33 @@
 package session
 
 import (
-   "github.com/jtumidanski/atlas-socket/crypto"
-   "log"
-   "net"
+	"github.com/jtumidanski/atlas-socket/crypto"
+	"log"
+	"net"
 )
 
 type Session interface {
-   ReceiveAESOFB() *crypto.AESOFB
-   WriteHello()
-   Disconnect()
+	SessionId() int
+	ReceiveAESOFB() *crypto.AESOFB
+	WriteHello()
+	Disconnect()
 }
 
 type Creator interface {
-   Create(*log.Logger, int, net.Conn) (Session, error)
+	Create(*log.Logger, int, net.Conn) (Session, error)
 }
 
 type Destroyer interface {
-   Destroy(int)
+	Destroy(int)
 }
 
 type Retriever interface {
-   Get(int) Session
-   GetAll() []Session
+	Get(int) Session
+	GetAll() []Session
 }
 
 type Service interface {
-   Creator
-   Retriever
-   Destroyer
+	Creator
+	Retriever
+	Destroyer
 }
